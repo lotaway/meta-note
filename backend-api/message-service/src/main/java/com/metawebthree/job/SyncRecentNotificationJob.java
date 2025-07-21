e123a2a4a79d578cfd3230af86d8d5aa6e0bab01
lotaway <576696294@qq.com>
2025-07-21 10:18:55 +0800

    Add job starter.

--- backend-api/message-service/src/main/java/com/metawebthree/job/SyncRecentNotificationJob.java
+++ backend-api/message-service/src/main/java/com/metawebthree/job/SyncRecentNotificationJob.java
@@ -1,5 +1,7 @@
 package com.metawebthree.job;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -9,15 +11,26 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class SyncRecentNotificationJob implements Job {
-    public static void create() {
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+    public static Boolean start() throws InterruptedException, ExecutionException {
+        try (ThreadPoolExecutor executor = new ThreadPoolExecutor(
             1, // core pool size
             8, // maximum pool size
             60L, // keep-alive time for idle threads
             TimeUnit.SECONDS, // time unit for keep-alive
             new LinkedBlockingQueue<Runnable>(1000) // work queue
-        );
-        // @todo for test
+        )) {
+            // @todo for test
+            Future<Boolean> task = executor.submit(() -> {
+                try {
+                    new SyncRecentNotificationJob().execute(null);
+                    return true;
+                } catch (JobExecutionException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            });
+            return task.get();
+        }
     }
 
     @Override
