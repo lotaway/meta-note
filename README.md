eaf0e8bca2b36a58ba1ad9ce9d20ead77e8fb342
lotaway <576696294@qq.com>
2025-07-23 15:29:09 +0800

    Remove unused backend-api.

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 
 ## Direct 目录说明
 
-* backend-api 服务端（业务处理、储存数据、前端接口）
 * system-management 桌面端管理（管理、笔记、通讯）
 * system-support 系统支持（提供客户端和服务端支持）
 
--- backend-api/product-service/src/main/java/com/metawebthree/utils/BucketChooseMethod.java
+++
@@ -1,9 +0,0 @@
-package com.metawebthree.utils;
-
-public enum BucketChooseMethod {
-    BY_ROUND_ROBIN,
-    BY_RANDOM,
-    BY_WEIGHT_ROUND_ROBIN,
-    BY_WEIGHT_RANDOM,
-    BY_LEAST_CONNECTS,
-}
